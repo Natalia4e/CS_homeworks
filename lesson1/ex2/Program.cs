c88a120048500c5624a0ac1ce433e1e27915e10e
Natalia Cherniavskaia <natatata931@mail.ru>
2022-10-05 20:31:15 +0300

    homework done

--- lesson1/ex2/Program.cs
+++ lesson1/ex2/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b & a > c)
+{
+    Console.WriteLine($"Число {a} наибольшее.");
+}
+else if (b > c & b > a)
+{
+    Console.WriteLine($"Число {b} наибольшее.");
+}
+else
+{
+    Console.WriteLine($"Число {c} наибольшее.");
+}
