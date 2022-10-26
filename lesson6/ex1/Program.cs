4807594402c29261c964c3fb14f931ad471777ea
Natalia Cherniavskaia <natatata931@mail.ru>
2022-10-26 19:50:35 +0300

    Homework done

---
+++ lesson6/ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Разделить ввод данных, от расчета данных (Одна функция вводит данные, другая функция подсчитывает количество положительных)
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    return number;
+}
+
+string message = Prompt ("Введите числа: ");
+
+int Count(string msg)
+{
+    string[] arr = message.Split(",");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i].Trim();
+    }
+
+    int count = 0;
+    for(int i = 0;i<arr.Length;i++)
+    {
+        int num = int.Parse(arr[i]);
+        if(num > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine(Count(message));
---
+++ lesson6/ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int b1 = Prompt ("Введите число b1 ");
+int k1 = Prompt ("Введите число k1 ");
+int b2 = Prompt ("Введите число b2 ");
+int k2 = Prompt ("Введите число k2 ");
+
+int x = (b2 - b1)/ (k1 - k2);
+int y = k1* (b2 - b1)/ (k1 - k2) + b1;
+
+ System.Console.WriteLine($"Пересечение в точке = ( {x} ; {y}) ");
